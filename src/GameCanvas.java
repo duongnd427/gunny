4330e911e0fb55f4a45eab822d1d9b2becd5170d
tititixxx <duong427@gmail.com>
2018-06-10 13:46:36 +0700

    gunny

---
+++ src/GameCanvas.java
@@ -0,0 +1,47 @@
+import base.GameObjectManager;
+import game.background.Background;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+
+public class GameCanvas extends JPanel {
+    BufferedImage backBuffered;
+    Graphics graphics;
+
+
+    public GameCanvas() {
+        this.setSize(1024, 600);
+        this.setupBackBuffered();
+        this.setupCharacter();
+        this.setVisible(true);
+    }
+
+    private void setupBackBuffered() {
+        this.backBuffered = new BufferedImage(1024, 600, BufferedImage.TYPE_4BYTE_ABGR);
+        this.graphics = this.backBuffered.getGraphics();
+    }
+
+    private void setupCharacter() {
+        GameObjectManager.instance.add(new Background());
+    }
+
+
+    @Override
+
+    protected void paintComponent(Graphics g) {
+        g.drawImage(this.backBuffered, 0, 0, null);
+    }
+
+    public void renderAll() {
+        GameObjectManager.instance.renderAll(this.graphics);
+        this.repaint();
+    }
+
+    public void runAll() {
+        GameObjectManager.instance.runAll();
+    }
+
+}
---
+++ src/base/GameObjectManager.java
@@ -0,0 +1,59 @@
+package base;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameObjectManager {
+
+    public static GameObjectManager instance = new GameObjectManager();
+
+    private List<GameObject> list;
+    private List<GameObject> tempList;
+
+    private GameObjectManager() {
+        this.list = new ArrayList<>();
+        this.tempList = new ArrayList<>();
+
+    }
+
+    public void add(GameObject gameObject) {
+        this.tempList.add(gameObject);
+    }
+
+    public void runAll() {
+        this.list
+                .stream()
+                .filter(gameObject -> gameObject.isAlive)
+                .forEach(gameObject -> gameObject.run());
+        this.list.addAll(this.tempList);
+        this.tempList.clear();
+    }
+
+    public void renderAll(Graphics graphics) {
+        this.list
+                .stream()
+                .filter(gameObject -> gameObject.isAlive)
+                .forEach(gameObject -> gameObject.render(graphics));
+    }
+
+    public <T extends GameObject> T recycle(Class<T> cls) {
+        T object = (T) this.list
+                .stream()
+                .filter(gameObject -> !gameObject.isAlive)
+                .filter(gameObject -> cls.isInstance(gameObject))
+                .findFirst()
+                .orElse(null);
+        if (object != null) {
+            object.isAlive = true;
+        } else {
+            try {
+                object = cls.newInstance();
+                this.add(object);
+            } catch (InstantiationException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return object;
+    }
+
+}
